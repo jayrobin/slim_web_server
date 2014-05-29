dc16ac7ad48d225c1d28307cbd23179413d0905d
James Robinson <james.michael.robinson@googlemail.com>
2014-05-29 16:25:17 +0100

    refactor server into class

--- server.rb
+++ server.rb
@@ -1,23 +1,33 @@
 require 'socket'
 
-def get_http_method(header)
-	header.scan(/^(.*) .* HTTP/)[0]
-end
+class Server
+	def initialize(port)
+		@port = port
+	end
 
-def get_resource(header)
-	header.scan(/^.* (.*) HTTP/)[0]
-end
+	def run
+		server = TCPServer.open(@port)
 
-port = 3001
-server = TCPServer.open(port)
+		loop do
+			Thread.start(server.accept) do |client|
+				header = client.read_nonblock(256)
 
-loop do
-	Thread.start(server.accept) do |client|
-		header = client.read_nonblock(256)
+				method = get_http_method(header)
+				resource = get_resource(header)
 
-		method = get_http_method(header)
-		resource = get_resource(header)
+			  client.close
+			end
+		end
+	end
 
-	  client.close
+	def get_http_method(header)
+		header.scan(/^(.*) .* HTTP/)[0]
+	end
+
+	def get_resource(header)
+		header.scan(/^.* (.*) HTTP/)[0]
 	end
 end
+
+server = Server.new(3001)
+server.run
